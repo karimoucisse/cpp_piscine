2d51b952f40d5ab4b1ccb48887f88c85cc754214
karimou cisse <karimou.cisse1@gmail.com>
2025-04-25 18:10:18 +0200

    change ex00 folder exercice

--- cpp00/ex00/main.cpp
+++ cpp00/ex00/main.cpp
@@ -5,15 +5,9 @@ int main(int ac, char **av)
 	if(ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else{
-
 		for(int i = 1; av[i]; i++)
-		{
 			for (int j = 0; av[i][j]; j++)
-			{
-				char c = toupper(av[i][j]);
-				std::cout << (char)toupper(c);
-			}
-		}
+				std::cout << (char)toupper(av[i][j]);
 	}
 	std::cout << std::endl;
 }
