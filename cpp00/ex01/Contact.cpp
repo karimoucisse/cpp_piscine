6ff142230f06ed18b836e175e311277968386487
Karimou Cisse <kcisse@made-f0Ar7s3.clusters.42paris.fr>
2025-04-27 18:55:53 +0200

    add change in code

---
+++ cpp00/ex01/Contact.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Contact.hpp"
+
+Contact::Contact(void){
+	this->first_name = "";
+	this->last_name = "";
+	this->nickname = "";
+	this->phone_number = "";
+	this->darkest_secret = "";
+}
