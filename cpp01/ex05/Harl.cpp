d70b260b5e8fa7fd0a0510da163058d99db7b05a
karimou cisse <karimou.cisse1@gmail.com>
2025-05-03 20:24:02 +0200

    add ex05 of cpp01

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,52 @@
+#include "Harl.hpp"
+
+void Harl::debug( void ){
+	std::cout << "I love having extra bacon for my 7XL-double"
+		<< "-cheese-triple-pickle-specialketchup burger. I really do!"
+		<< std::endl;
+}
+
+void Harl::info( void ){
+	std::cout << "I cannot believe adding extra bacon costs more money. "
+		<< "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+		<< std::endl;
+}
+
+void Harl::warning( void ){
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for"
+ 		<< "years, whereas you started working here just last month."
+		<< std::endl;
+}
+
+void Harl::error( void ){
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+
+Harl::Harl(){
+	return;
+}
+
+Harl::~Harl(){
+	return;
+}
+
+void Harl::complain( std::string level ) {
+	std::string levels[] = {"debug", "info", "warning", "error"};
+	int length = sizeof(levels) / sizeof(levels[0]);
+
+	void (Harl::*f[])(void) = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error,
+	};
+
+	for(int i = 0; i < length ; i++)
+	{
+		if(levels[i] == level)
+			(this->*f[i])();
+		return ;
+	}
+	std::cout << "Unknown function !" << std::endl;
+}
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,7 @@
+#include "Harl.hpp"
+
+int main(void){
+	Harl call;
+
+	call.complain("debug");
+}
