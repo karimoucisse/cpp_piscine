7e7c9ea77942b2377743e1751dc74a6c779624b9
karimou cisse <karimou.cisse1@gmail.com>
2025-05-04 15:35:57 +0200

    add cpp02 folder

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,7 @@
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() { return; }
+
+Fixed::~Fixed() { return; }
+
---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,13 @@
+#ifndef FIXED_HPP_
+#define FIXED_HPP_
+
+class Fixed {
+	public:
+		Fixed();
+		~Fixed();
+	private:
+
+};
+
+#endif
+
